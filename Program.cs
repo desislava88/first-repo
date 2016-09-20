356db041ea401245dc851776c1a20b71432e8b4c
desislava88 <desitostefanova@gmail.com>
2016-09-20 22:16:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = n;
+            int sum = 0;
+            do
+            {
+                int current = number % 10;
+                sum += current;
+                number /= 10;    
+            } while (number != 0);
+            Console.WriteLine("Sum is " + sum);
+        }
+    }
+}
